fe2a6b0b1a8eb5d7d9973ab7c3498e9a61d9db77
Samuel Attard <sattard@atlassian.com>
2018-11-01 17:57:20 +1100

    fix: send NSView* as the response to getNativeWindowHandle() instead of a null handle

--- atom/browser/api/atom_api_top_level_window.cc
+++ atom/browser/api/atom_api_top_level_window.cc
@@ -691,8 +691,8 @@ void TopLevelWindow::SetBrowserView(v8::Local<v8::Value> value) {
 }
 
 v8::Local<v8::Value> TopLevelWindow::GetNativeWindowHandle() {
-  gfx::AcceleratedWidget handle = window_->GetAcceleratedWidget();
-  return ToBuffer(isolate(), static_cast<void*>(&handle), sizeof(handle));
+  auto handle = window_->GetNativeWindowHandlePointer();
+  return ToBuffer(isolate(), std::get<0>(handle), std::get<1>(handle));
 }
 
 void TopLevelWindow::SetProgressBar(double progress, mate::Arguments* args) {
